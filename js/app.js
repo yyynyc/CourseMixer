d05594eeb971e0e58c8ad055d4c87eeac59a91e0
Robin Yang <yyynyc@gmail.com>
2014-08-06 09:58:06 -0400

    got course page working but modules do not pull in data

--- js/app.js
+++ js/app.js
@@ -8,12 +8,17 @@ App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.Router.map(function() {
   this.resource('courses');
-  // this.resource('course', {path: 'courses/:course_id'});
-  this.resource('course', {path: 'courses/:course_id'}, function() {
+  this.resource('course', {path: '/courses/:course_id'}, function(){
   	this.resource('modules', function(){
-  		this.resource('module', {path: 'modules/:module_id'});
+ 		this.resource('module', {path: ':module_id'})
   	});
   });  
+});
+
+App.IndexRoute = Ember.Route.extend({
+	redirect: function() {
+		this.transitionTo('courses');
+	}
 });
 
 App.CoursesRoute = Ember.Route.extend({
@@ -23,36 +28,46 @@ App.CoursesRoute = Ember.Route.extend({
 });
 
 App.CourseRoute = Ember.Route.extend({
-	model: function(pramas){
-		return this.store.findBy('id', pramas.course_id);
+	redirect: function(){
+		this.transitionTo('modules')
 	}
 });
 
 App.ModulesRoute = Ember.Route.extend({
 	model: function(){
-		var course = this.modelFor('course');
-		return course.modules.find('module');
+		// var course = this.modelFor('course')
+		// return course.modules.find('module');
+		// return modules;
+		// var modules = this.store.find('module', { course: this.modelFor('course').get('id') }).then(function(module) {
+		// 	console.log(module);
+		// });
+		// return this.store.filter('module', {course: this.modelFor('course').get('id')}, function(module){
+		// 	return module.get('course')
+		// });
+		return this.modelFor('course').get('modules')
 	}
+	// afterModel: function(model) {
+	// 	debugger;
+	// }
 });
 
-App.ModuleRoute = Ember.Route.extend({
-	model: function(){
-		return modules.findBy('id', pramas.module_id);
-	}
+App.ModulesController = Ember.ArrayController.extend({
+    needs: "course",
+    course: Ember.computed.alias("controllers.course.model")
 });
 
-App.UsersRoute = Ember.Route.extend({
-	model: function(){
-		return users;
-	}
+DS.FixtureAdapter.reopen({
+  queryFixtures: function(records, query, type) {
+    return records.filter(function(record) {
+        for(var key in query) {
+            if (!query.hasOwnProperty(key)) { continue; }
+            var value = query[key];
+            if (record[key] !== value) { return false; }
+        }
+        return true;
+    });
+  }
 });
-
-App.UserRoute = Ember.Route.extend({
-	model: function(){
-		return users.findBy('id', pramas.user_id);
-	}
-});
-
 
 App.CoursesController = Ember.ArrayController.extend({
 	viewedOn: function(){
@@ -65,31 +80,31 @@ App.Course = DS.Model.extend({
 	image_url: DS.attr('string'),
 	duration: DS.attr('string'),
 	description: DS.attr('string'),
-	modules: DS.hasMany('module')
+	modules: DS.hasMany('module', {async: true})
 });
 
 App.Module = DS.Model.extend({
-	course: DS.belongsTo('course'),
+	course: DS.belongsTo('course', {async: true}),
 	name: 'String',
 	level: 'string'
 });
 
 App.Course.FIXTURES = [{
-	id: '1', 
+	id: 1, 
 	title: "Imagination",
 	image_url: "/images/flickr_imagination.jpg",
 	duration: "3 classes",
 	description: "Visualization, discussions, and activities to foster imagination.",
 	modules: [1, 2, 3, 4, 5, 6, 7, 8]
 },{
-	id: '2',
+	id: 2,
 	title: "Creativity",
 	image_url: "/images/flickr_creativity.jpg",
 	duration: "5 classes",
 	description: "Projects and activities to foster creativity.",
 	modules: []
 },{
-	id: '3',
+	id: 3,
 	title: "Critical Thinking",
 	image_url: "/images/flickr_think.jpg",
 	duration: "4 classes", 
@@ -97,71 +112,51 @@ App.Course.FIXTURES = [{
 	modules: []
 }];
 
+// var modules = [{
 App.Module.FIXTURES = [{
 	id: 1,
 	course: 1,
 	name: "C1R1",
-	level: "remediation"
+	level: "remediation",
+	description: "Lorem ipsum aliquet risus orci curae etiam quam class urna, tempor libero litora laoreet ipsum a pellentesque tristique, et condimentum adipiscing dui tempus duis orci rhoncus suscipit tellus gravida at ipsum."
 },{
 	id: 2,
 	course: 1,
 	name: "C1R2",
-	first: "remediation"
+	level: "remediation",
+	description: "Aenean porta bibendum ornare id tortor dictum auctor praesent est, nam nullam aliquam tempor mattis metus lacus quisque, diam habitasse condimentum senectus a varius vivamus ultricies nec elementum primis amet."
 },{
 	id: 3,
 	course: 1,
 	name: "C1S1",
-	first: "Simple"
+	level: "Simple",
+	description: "Quisque metus hac vehicula inceptos mattis per facilisis eu ligula blandit fringilla mi, pulvinar mauris sodales rutrum non ornare convallis interdum varius orci rutrum."
 },{
 	id: 4,
 	course: 1,
 	name: "C1S2",
-	first: "Simple"
+	level: "Simple", 
+	description: "Lectus pharetra blandit fermentum massa quisque sem ullamcorper turpis id mollis tristique aliquam, maecenas vehicula augue vel senectus velit litora dapibus ligula tincidunt."
 },{
 	id: 5,
 	course: 1,
 	name: "C1M1",
-	first: "Medium"
+	level: "Medium",
+	description: "Hendrerit est arcu curabitur malesuada litora nostra dictumst sit ligula quisque, curabitur viverra imperdiet risus enim est id arcu sed scelerisque, curae habitasse integer egestas litora odio sem pulvinar eget."
 },{
 	id: 6,
 	course: 1,
 	name: "C1M2",
-	first: "Medium"
+	level: "Medium"
 },{
 	id: 7,
 	course: 1,
 	name: "C1A1",
-	first: "Advanced"
+	level: "Advanced"
 },{
 	id: 8,
 	course: 1,
 	name: "C1A2",
-	first: "Advanced"
-}];
-
-var users = [{
-	id: 1,
-	first: "Abe"
-},{
-	id: 2,
-	first: "Ben"
-},{
-	id: 3,
-	first: "Charlie"
-},{
-	id: 4,
-	first: "Donna"
-},{
-	id: 5,
-	first: "Ellen"
-},{
-	id: 6,
-	first: "Frank"
-},{
-	id: 7,
-	first: "Gary"
-},{
-	id: 8,
-	first: "Helen"
+	level: "Advanced"
 }];
 
